a7605d77730f285f77388da7b641d112344d3071
김남호 <kimnamo0113@gmail.com>
2019-09-10 16:40:14 +0900

    ex03

---
+++ src/main/java/com/yi/controller/UploadController.java
@@ -0,0 +1,211 @@
+package com.yi.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+			
+	private String innerUploadPath = "resources/upload";
+	
+	@Resource(name="uploadPath")
+	private String outUploadPath;
+	
+	
+	@RequestMapping(value="/innerUpload", method=RequestMethod.GET)
+	public String innerUploadForm() {
+		logger.info("-----------------------------innerUpload GET");
+		return "innerUploadForm";
+	}
+	
+	@RequestMapping(value="/innerUpload", method=RequestMethod.POST)
+	public String innerUploadPost(String test, MultipartFile file, HttpServletRequest request, Model model) throws IOException {
+		logger.info("-----------------------------innerUpload POST");
+		logger.info("test="+test);
+		logger.info("file="+file.getOriginalFilename());
+		logger.info("file size="+file.getSize());
+		
+		String root_path = request.getSession().getServletContext().getRealPath("/"); //ex03서버
+		// ex/03/resources/upload
+		File dirPath = new File(root_path+"/"+innerUploadPath);
+		if(dirPath.exists()==false) {//폴더 없음
+			dirPath.mkdir();
+		}
+		//빈 껍데기 파일이 만들어짐
+		UUID uid = UUID.randomUUID();
+		String savedName = uid+"_"+file.getOriginalFilename();
+		File target = new File(root_path+"/"+innerUploadPath,savedName); 
+		FileCopyUtils.copy(file.getBytes(), target); //파일 업로드 완료
+		
+		model.addAttribute("test", test);
+		model.addAttribute("file", savedName);
+		
+		
+		
+		return "innerUploadResult";
+	}
+	
+	
+	@RequestMapping(value="/innerMultiUpload", method=RequestMethod.GET)
+	public String innerMultiUploadForm() {
+		logger.info("-----------------------------innerMultiUpload GET");
+		return "innerMultiUploadForm";
+	}
+	@RequestMapping(value="/innerMultiUpload", method=RequestMethod.POST)
+	public String innerMultiUploadFormPOST(String test, List<MultipartFile> files, HttpServletRequest request, Model model) throws IOException {
+		logger.info("-----------------------------innerMultiUpload POST");
+		logger.info("test = "+test);
+		for(MultipartFile file :files) {
+			logger.info(file.getOriginalFilename());
+			logger.info(file.getSize()+"");
+			logger.info(file.getContentType());
+		}
+		
+		String root_path = request.getSession().getServletContext().getRealPath("/");
+		File dir = new File(root_path+"/"+innerUploadPath);
+		if(dir.exists()==false) {
+			dir.mkdir();
+		}
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		for(MultipartFile file:files) {
+			UUID uid = UUID.randomUUID();
+			String savedName = uid.toString()+"_"+file.getOriginalFilename();
+			
+			File target = new File(root_path+"/"+innerUploadPath,savedName);
+			FileCopyUtils.copy(file.getBytes(), target);
+			list.add(savedName);
+		}
+		
+		model.addAttribute("test",test);
+		model.addAttribute("list",list);
+		
+		return "innerMultiUploadResult";
+	}
+	
+	
+	@RequestMapping(value="/outUpload", method=RequestMethod.GET)
+	public String outUploadGET() {
+		logger.info("-----------------------------outUpload GET");
+		return "outUploadForm";
+	}
+	@RequestMapping(value="/outUpload", method=RequestMethod.POST)
+	public String outUploadPOST(String test,MultipartFile file,Model model) throws IOException {
+		logger.info("-----------------------------outUpload POST");
+		logger.info("test:"+test);
+		logger.info("file:"+file.getOriginalFilename());
+		logger.info("file size:"+file.getSize());
+		
+		UUID uid = UUID.randomUUID();
+		String savedName = uid+"_"+file.getOriginalFilename();
+		File target = new File(outUploadPath,savedName); //outUploadPath경로가 반드시 존재한다는 가정하게 처리됨
+		FileCopyUtils.copy(file.getBytes(), target);
+		
+		model.addAttribute("test",test);
+		model.addAttribute("file",savedName);
+		
+		return "outUploadResult";
+	}
+	@RequestMapping(value="/displayFile",method=RequestMethod.GET)
+	public @ResponseBody ResponseEntity<byte[]> diplayFile(String filename){
+		logger.info("----------displayFile,filename="+filename);
+		
+		String formatName = filename.substring(filename.lastIndexOf(".")+1);//확장자만 뽑아냄
+		MediaType mType = null;
+		ResponseEntity<byte[]> entity;
+		
+		if(formatName.equalsIgnoreCase("jpg")) {
+			mType=MediaType.IMAGE_JPEG;
+		}
+		else if(formatName.equalsIgnoreCase("gif")) {
+			mType=MediaType.IMAGE_GIF;
+		}
+		else if(formatName.equalsIgnoreCase("png")) {
+			mType=MediaType.IMAGE_PNG;
+		}
+		
+		try {
+		HttpHeaders headers = new HttpHeaders();
+		InputStream in = new FileInputStream(outUploadPath+"/"+filename);
+		headers.setContentType(mType);
+		
+		entity = new ResponseEntity<byte[]>(
+				IOUtils.toByteArray(in),
+				headers,
+				HttpStatus.CREATED
+				);
+		}catch (Exception e) {
+			e.printStackTrace();
+			entity=new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+	}
+	
+	
+	@RequestMapping(value="/dragUpload", method=RequestMethod.GET)
+	public String dragUploadGET() {
+		logger.info("-----------------------------dragUpload GET");
+		return "dragDropForm";
+	}
+	
+	@RequestMapping(value="/dragUpload", method=RequestMethod.POST)
+	public ResponseEntity<String> dragUploadPOST(List<MultipartFile> files, String test) {
+		logger.info("-----------------------------dragUploadPOST");
+		ResponseEntity<String> entity = null;
+		logger.info("test:"+test);
+		for(MultipartFile file:files) {
+			logger.info(file.getOriginalFilename());
+			logger.info(file.getSize()+"");
+		}
+		
+		try {
+			for(MultipartFile file : files) {
+				UUID uid = UUID.randomUUID();
+				String savedName = uid.toString()+"_"+file.getOriginalFilename();
+				File target = new File(outUploadPath,savedName);
+				FileCopyUtils.copy(file.getBytes(), target);
+			}
+			entity = new ResponseEntity<String>("success",HttpStatus.OK);
+		}catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>("fail",HttpStatus.BAD_REQUEST);
+		}
+		
+		
+		return entity;
+	}
+	
+}
+
+
+
---
+++ src/main/webapp/WEB-INF/views/dragDropForm.jsp
@@ -0,0 +1,84 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+
+<style type="text/css">
+	#dropBox{
+		min-width:400px;
+		min-height: 300px;
+		height:auto;
+		width:400px;
+		border: 1px dotted gray;
+	}
+	
+	#dropBox img{
+		width:100px;
+		height: 100px;
+	}
+</style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+</head>
+<body>
+	<form id="f1" action="dragUpload" method="post" enctype="multipart/form-data">
+		<input type="text" name="test">
+		<input type="submit">
+	</form>
+	<div id="dropBox"></div>
+	
+	<script type="text/javascript">
+		var formData = new FormData();//서버로 보낼 데이터를 담을 공간
+		
+		$("#f1").submit(function(e){
+			e.preventDefault(); //ajax로 처리하므로, submit 안되게 막음
+			formData.append("test",$("input[name='test']").val());
+			$.ajax({
+				url:"dragUpload",
+				type:"post",
+				data:formData,
+				processData:false, //FormData 를 보낼 경우 processData:false, contentType:false처리 필요
+				contentType:false,
+				dataType:"text",
+				success:function(res){
+					console.log(res);
+					
+				}
+			})
+		})
+		
+		$("#dropBox").on("dragenter dragover",function(event){
+			event.preventDefault();
+		})
+	
+		$("#dropBox").on("drop",function(event){
+			event.preventDefault();
+			
+			var files = event.originalEvent.dataTransfer.files;
+			var file=files[0];
+			console.log(file);
+			
+			var reader = new FileReader();
+			reader.addEventListener("load", function(e) {
+				var $img = $("<img>").attr("src",reader.result);
+				$("#dropBox").append($img);
+			}, false)
+			
+			if(file){
+				reader.readAsDataURL(file);
+			}
+			
+			formData.append("files",file);//files=[file,file,file]
+			console.log(formData.getAll)
+		})
+		
+		
+		
+	</script>
+	
+</body>
+</html>
